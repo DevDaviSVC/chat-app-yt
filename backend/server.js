2700ea5bc9e6cc3036bd8b6aa9b155ffd6313562
Davi Souza <davisouzavieiradacosta@gmail.com>
2024-08-31 17:01:50 -0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from 'path';
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -14,6 +15,7 @@ import { connectToMongoDb } from "./db/connectToMongoDb.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body) allows json in requests
@@ -25,6 +27,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // turn server on
 server.listen(PORT, () => {
