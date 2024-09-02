d4a24374b5e30f3f2c4c66d035e8687d254642b6
Davi Souza <davisouzavieiradacosta@gmail.com>
2024-09-01 21:51:40 -0300

    Import error fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/SocketCOntext.jsx'
+import { SocketContextProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
